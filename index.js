9222c1ae587148f703f3ab9e76ac641f8a97d40a
Ashish <ashishsark98@gmail.com>
2022-06-21 21:47:25 +0530

    fixed toggle icon issue

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 $(function(){ 
     $(".dropdown-toggle").click(function(){ 
-   $(".arrow").attr('src',  
-                ($("img").attr('src') == 'images/icon-arrow-down.svg'  
+        console.log($($(this).children()[0]).attr("src"));
+        $($(this).children()[0]).attr('src',  
+                ($($(this).children()[0]).attr('src') == 'images/icon-arrow-down.svg'  
                     ? 'images/icon-arrow-up.svg'  
                     : 'images/icon-arrow-down.svg' 
                      ) 
